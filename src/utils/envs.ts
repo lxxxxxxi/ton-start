d813118c649f5b92f8fecfe9a947c75421f57d00
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-06-29 17:34:00 +0800

    Update env

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -1,11 +1,11 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const GITHUB_REPOSITORY = import.meta.env.VITE_GITHUB_REPOSITORY;
-export const DESTINATION_ADDRESS = import.meta.env.VITE_DESTINATION_ADDRESS;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || process.env.VITE_API_BASE_URL;
+export const DESTINATION_ADDRESS =
+    import.meta.env.VITE_DESTINATION_ADDRESS || process.env.VITE_DESTINATION_ADDRESS;
 
 if (!API_BASE_URL) {
     throw new Error("VITE_API_BASE_URL is not defined");
 }
 
-if (!GITHUB_REPOSITORY) {
-    throw new Error("VITE_GITHUB_REPOSITORY is not defined");
+if (!DESTINATION_ADDRESS) {
+    throw new Error("DESTINATION_ADDRESS is not defined");
 }
