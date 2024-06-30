d8dcb59c0cb0f868885695de9085981602b0eeed
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-06-30 12:11:05 +0800

    Update

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -1,6 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || process.env.VITE_API_BASE_URL;
-export const DESTINATION_ADDRESS =
-    import.meta.env.VITE_DESTINATION_ADDRESS || process.env.VITE_DESTINATION_ADDRESS;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const DESTINATION_ADDRESS = import.meta.env.VITE_DESTINATION_ADDRESS;
 
 if (!API_BASE_URL) {
     throw new Error("VITE_API_BASE_URL is not defined");
