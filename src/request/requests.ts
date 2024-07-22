dab6e3bd8b2182937d16ef0477e3c09c322bcd91
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-07-22 09:25:37 +0800

    refactor: Remove unused import of API_BASE_URL2 in requests.ts

--- src/request/requests.ts
+++ src/request/requests.ts
@@ -3,7 +3,6 @@ import { TelegramUser } from "../pages/AccountCenter/TelegramLoginButton";
 import apiRequest from "./apiRequest";
 import axios from "axios";
 import { errorMonitor } from "events";
-import { API_BASE_URL2 } from "@/utils/envs";
 
 export const getExchangeRate = () => {
     return axios.get("https://api.coinbase.com/v2/exchange-rates?currency=USDT");
