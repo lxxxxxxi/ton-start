bab171e8fdc2f1ea9c8ff16b614046eff9479f5e
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-07-22 09:11:58 +0800

    refactor: Update API base URLs and constants for test environment

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,8 +1,14 @@
-export const USDT_MASTER_ADDRESS = "kQBu2entjYBvkcmv7CSXtwwZ0rXgAvOHEGuERIdwQIh64aRl";
-export const RECHARGE_MAX = 100;
+// main
+export const USDT_MASTER_ADDRESS = "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs";
+export const RECHARGE_MAX = 1000;
 export const RECHARGE_MIN = 10;
-// export const WITHDRAW_MAX =
-export const WITHDRAW_MIN = 10;
+export const WITHDRAW_MIN = 100;
+
+// test
+// export const USDT_MASTER_ADDRESS = "kQBu2entjYBvkcmv7CSXtwwZ0rXgAvOHEGuERIdwQIh64aRl";
+// export const RECHARGE_MAX = 100;
+// export const RECHARGE_MIN = 10;
+// export const WITHDRAW_MIN = 10;
 
 // 充值 —— 测试环境10到100，正式环境100到50000
 // 提现 —— 测试环境最低10元 正式环境最低100
